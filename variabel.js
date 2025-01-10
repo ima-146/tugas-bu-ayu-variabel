731caf42f149c7e5420c41ce15b2a56b7d9bf4cf
ima-146 <sitifatimahaz33@gmail.com>
2025-01-10 10:30:57 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,48 @@
+let nama = "siti fatimah azzahroh" ;
+console.log(nama);
+
+let alamat = "Daleman Rt04/01 Gemulak Kec.Sayung Kab.Demak";
+console.log(alamat);
+
+let usia = "16 Tahun";
+console.log(usia);
+
+
+let TTL = "Demak 31 Oktober 2009";
+console.log(TTL);
+
+let gender = "perempuan";
+console.log(gender);
+
+let agama = "islam";
+console.log(agama);
+
+let email = "sitifatimahaz33@gmail.com";
+console.log(email);
+
+let beratbadan = "50 kg";
+console.log(beratbadan);
+
+let tinggibadan = "157 cm";
+console.log(tinggibadan);
+
+let hobi = "menonton tv";
+console.log(hobi);
+
+let cita = "menjadi orang sukses";
+console.log(cita);
+
+let status = "pelajar";
+console.log(status);
+
+let sekolah = "smk n 1 sayung";
+console.log(sekolah);
+
+
+let jurusan = "pplg";
+console.log(jurusan);
+
+document.write("nama saya adalah"+ nama + "alamat saya di" + alamat + "tempat tanggal lahir saya" + TTL + "usia saya"
+     + usia + "gender saya" + gender + "agama saya" + agama + "email saya" + email + "beratbadan saya" + beratbadan +
+      "tinggibadan saya" + tinggibadan + "hobi saya" + hobi + "cita cita saya" + cita + "status" + status + 
+      "saya sekolah di" + sekolah + "dan jurusan saya adalah" + jurusan)
